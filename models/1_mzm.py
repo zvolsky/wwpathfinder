0231172098651bae3a7653f431510e3c16205a49
zvolsky <zvolsky@seznam.cz>
2017-01-05 15:54:16 +0100

    initial

---
+++ models/1_mzm.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
+from plugin_mz import link
+
+if request.is_local:
+    from gluon.custom_import import track_changes
+    track_changes(True)    # auto-reload modules
+elif request.is_https:
+    session.secure()
+
+link('css/mz-bootstrap3.css')
---
+++ models/db_model.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+'''
+db.define_table(tbl,
+        Field(fld, 'string', length=32,
+              label=T("xxx"), comment=T("xxx")),
+        )
+'''
